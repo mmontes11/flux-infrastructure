1be3ec25b517b0f2aee24bd5807051c39fc98390
Martín Montes <martin11lrx@gmail.com>
2022-03-19 17:35:44 +0100

    Added infra

---
+++ scripts/bootstrap.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+flux bootstrap github \
+  --owner=$GITHUB_USER \
+  --repository=fleet-infra \
+  --branch=main \
+  --path=./clusters/my-cluster \
+  --personal \
+  --private \
+  --network-policy 
